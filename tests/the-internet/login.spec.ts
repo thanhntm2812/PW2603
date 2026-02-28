ef709de80e0d271d4b650066c4d75f98a8760e02
Nguyen Thi Mai Thanh <maithanh.nguyen@finviet.corp>
2026-02-28 15:03:51 +0700

    first commit

---
+++ tests/the-internet/login.spec.ts
@@ -0,0 +1,25 @@
+// 
+
+import { test, expect } from '@playwright/test';
+
+/**
+ * TC01: Form Authentication : Login successful with valid credentials
+
+Open browser
+Navigate to https://the-internet.herokuapp.com/login
+Fill in username with tomsmith
+Fill in the password with SuperSecretPassword!
+Click on Login button
+And the home page is appear
+ */
+test('Login successful with valid credentials', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.locator('html').click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+  await expect(page.getByText('You logged into a secure area')).toBeVisible();
+  await expect(page.locator('h4')).toContainText('Welcome to the Secure Area. When you are done click logout below.');
+});
