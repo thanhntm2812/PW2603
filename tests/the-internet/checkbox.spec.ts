c7c73adf6695f52cb8d83d258b0e29b8f85254cf
Nguyen Thi Mai Thanh <maithanh.nguyen@finviet.corp>
2026-02-28 19:18:16 +0700

    checkbox.spec.ts

--- tests/the-internet/checkbox.spec.ts
+++ tests/the-internet/checkbox.spec.ts
@@ -20,4 +20,12 @@ test('Check to a box', async ({ page }) => {
   //Assert that the checkbox is checked
   const locator = page.getByRole('checkbox').first();
   await expect(locator).toBeChecked();
+
+  // Check the checkbox 2
+  await page.getByRole('checkbox').nth(1).uncheck();
+
+  //Assert that the checkbox is checked
+  const locator2 = page.getByRole('checkbox').nth(1)
+  await expect(locator2).not.toBeChecked();
+  console.log('checkbox 2', locator2);
 });
